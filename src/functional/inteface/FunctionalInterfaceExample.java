595d1fd2de04c87ffe39bd17044310fd2b740892
Surreallistic <iluzion5@o2.pl>
2017-04-06 10:58:11 +0200

    functional interface

---
+++ src/functional/inteface/FunctionalInterfaceExample.java
@@ -0,0 +1,23 @@
+package functional.inteface;
+
+import difference.inteface.lambda.GreetingImpl;
+
+/**
+ * Created by Lukasz S. on 05.04.2017.
+ *
+ * Type Inference -
+ *
+ */
+public class FunctionalInterfaceExample {
+
+    void greet(Greeting greeting) {
+        greeting.perform();
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
---
+++ src/functional/inteface/Greeting.java
@@ -0,0 +1,9 @@
+package functional.inteface;
+
+/**
+ * Created by Lukasz S. on 05.04.2017.
+ */
+public interface Greeting {
+
+    void perform();
+}
