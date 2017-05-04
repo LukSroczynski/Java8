604de199fdb75d8fd35d8f659b32667ddf0911f2
Surreallistic <iluzion5@o2.pl>
2017-05-04 12:52:44 +0200

    oop vs lambda

---
+++ src/current/type/inference/TypeInferenceExample.java
@@ -0,0 +1,22 @@
+package current.type.inference;
+
+import current.type.inference.interfaces.StringLength;
+import current.type.inference.interfaces.StringLengthOfTwo;
+
+/**
+ * Created by Lukasz S. on 04.05.2017.
+ * inference - wnioskowanie, wniosek, konkluzja
+ */
+public class TypeInferenceExample {
+    public static void main(String[] args) {
+
+        StringLength stringLength = s -> s.length();
+
+        StringLengthOfTwo stringLengthOfTwo = (s1, s2) -> s1.length() + s2.length();
+
+        System.out.println(stringLength.getString(" b r o o o m "));
+
+        System.out.println(stringLengthOfTwo.getString("ONE", "X"));
+
+    }
+}
---
+++ src/current/type/inference/interfaces/StringLength.java
@@ -0,0 +1,8 @@
+package current.type.inference.interfaces;
+
+/**
+ * Created by Lukasz S. on 04.05.2017.
+ */
+public interface StringLength {
+    int getString(String s);
+}
